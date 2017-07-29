8253dff7631acd6fdf46216174c204be4f68f055
Weslley Santo <weslley.santo@gmail.com>
2017-07-28 23:14:39 -0300

    primeira etapa criação api

---
+++ apiRecupera.php
@@ -0,0 +1,35 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: text/html; charset=utf-8');
+
+$data = file_get_contents("php://input");
+$objData = json_decode($data);
+
+$counter = 0;
+$token = $objData->token;
+
+include_once 'includes/conexao.php';
+
+if ($token === "1f3d2gs3f2fg3as2fdg3re2t1we46er45" && isset($token)) {
+
+
+	$query = $con->prepare('SELECT * FROM tb_usuario LIMIT '.$counter.', 6');
+
+	$query->execute();
+
+	$retorno = array();
+
+	while($result = $query->fetch()){
+		$retorno[] 		= array(
+						'id' 	=> $result["cd_usuario"],
+						'nome' 	=> $result["nm_usuario"],
+						'email' => $result["email"],
+						'senha' 	=> $result["senha"]
+		);
+	}
+
+	echo(json_encode($retorno));
+}else{
+	die('Acesso negado!');
+}
+
