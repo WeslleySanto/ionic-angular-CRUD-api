c5561c483fe3a219c4493b450eb9857b8b78f462
Weslley Santo <weslley.santo@gmail.com>
2017-08-07 20:09:41 -0300

    ajustando o recupera e atualizando sftp

--- apiRecupera.php
+++ apiRecupera.php
@@ -5,15 +5,16 @@ header('Content-Type: text/html; charset=utf-8');
 $data = file_get_contents("php://input");
 $objData = json_decode($data);
 
-$counter = 0;
+$counter = $objData->counter;
 $token = $objData->token;
 
 include_once 'includes/conexao.php';
 
 if ($token === "1f3d2gs3f2fg3as2fdg3re2t1we46er45" && isset($token)) {
 
+	//echo("SELECT * FROM tb_usuario ORDER BY dt_cadastro DESC LIMIT {$counter}, 5");
 
-	$query = $con->prepare('SELECT * FROM tb_usuario ORDER BY dt_cadastro DESC LIMIT '.$counter.', 6');
+	$query = $con->prepare("SELECT * FROM tb_usuario ORDER BY dt_cadastro DESC LIMIT {$counter}, 5");
 
 	$query->execute();
 
